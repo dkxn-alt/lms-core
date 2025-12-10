0f2308d05e7bb066dab9658e18a683b6112bb3de
dkxn-alt <209535354+dkxn-alt@users.noreply.github.com>
2025-12-10 06:17:33 +0200

    Added DTO for members

---
+++ src/modules/members/dto/update-member.dto.ts
@@ -0,0 +1,23 @@
+import { IsString, IsEmail, IsOptional } from 'class-validator';
+
+export class UpdateMemberDto {
+  @IsOptional()
+  @IsString()
+  name: string;
+
+  @IsOptional()
+  @IsEmail()
+  email: string;
+
+  @IsOptional()
+  active: boolean;
+}
+
+// model members {
+//   id        String    @id @default(uuid()) @db.Uuid
+//   name      String
+//   email     String    @unique
+//   active    Boolean?  @default(true)
+//   joined_at DateTime? @default(now()) @db.Timestamptz(6)
+//   loans     loans[]
+// }
